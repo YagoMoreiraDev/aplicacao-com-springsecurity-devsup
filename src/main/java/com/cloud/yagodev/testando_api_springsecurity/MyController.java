58e7e796aee04a853a0483fc639da78f4cad7cc1
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-08-28 16:00:25 -0300

    Adicinado o arquivo .http para teste de algumas requisições | Teste das configs da classe SecurityConfig e liberação das rotas

---
+++ src/main/java/com/cloud/yagodev/testando_api_springsecurity/MyController.java
@@ -0,0 +1,17 @@
+package com.cloud.yagodev.testando_api_springsecurity;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class MyController {
+
+    @GetMapping
+    public ResponseEntity<String> mensagemTeste() {
+        return ResponseEntity.ok("Acesso liberado");
+    }
+}
